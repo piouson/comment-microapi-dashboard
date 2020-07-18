2f08076d3a84e420063cfff886b0647589bd90bf
piouson <piouson@gmail.com>
2020-07-18 09:37:30 +0100

    Updates

---
+++ precache-manifest.15c60e37699792fab1b34896f982734f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e735dae4181361bd3fd7d960594875d",
+    "url": "/comment-microapi-dashboard/index.html"
+  },
+  {
+    "revision": "234eeee8b433e337c6ce",
+    "url": "/comment-microapi-dashboard/static/js/2.76b217c1.chunk.js"
+  },
+  {
+    "revision": "f07796f4f30300629a9fa688e41af979",
+    "url": "/comment-microapi-dashboard/static/js/2.76b217c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce2774e773b21acbe2a4",
+    "url": "/comment-microapi-dashboard/static/js/main.531d2571.chunk.js"
+  },
+  {
+    "revision": "a5f66f04d694cfe6c43f",
+    "url": "/comment-microapi-dashboard/static/js/runtime-main.d5df5543.js"
+  }
+]);
